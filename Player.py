f11fc56165b29b98ffdec6cc31521fac6d31f588
Milan Zinzuvadiya <milanzinzuvadiya99@gmail.com>
2020-04-20 16:10:50 -0400

    OOAD : Player class create

---
+++ Player.py
@@ -0,0 +1,9 @@
+class Player():
+    def __init__(self):
+        self.CharacterSprites = {
+            'princess': pygame.image.load('Images/princess.png'),
+            'boy': pygame.image.load('Images/boy.png'),
+            'catgirl': pygame.image.load('Images/catgirl.png'),
+            'horngirl': pygame.image.load('Images/horngirl.png'),
+            'pinkgirl': pygame.image.load('Images/pinkgirl.png')
+        }

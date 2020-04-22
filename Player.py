50272a88c788dbd8919888ac0295bd9f52cdb73a
Milan Zinzuvadiya <milanzinzuvadiya99@gmail.com>
2020-04-21 21:35:59 -0400

    player update

--- Player.py
+++ Player.py
@@ -1,9 +1,13 @@
+import pygame
 class Player():
-    def __init__(self):
-        self.CharacterSprites = {
-            'princess': pygame.image.load('Images/princess.png'),
-            'boy': pygame.image.load('Images/boy.png'),
-            'catgirl': pygame.image.load('Images/catgirl.png'),
-            'horngirl': pygame.image.load('Images/horngirl.png'),
-            'pinkgirl': pygame.image.load('Images/pinkgirl.png')
+    CharacterSprites = {
+            'princess': pygame.image.load('Images/characters/princess.png'),
+            'boy': pygame.image.load('Images/characters/boy.png'),
+            'catgirl': pygame.image.load('Images/characters/catgirl.png'),
+            'horngirl': pygame.image.load('Images/characters/horngirl.png'),
+            'pinkgirl': pygame.image.load('Images/characters/pinkgirl.png')
         }
+
+    def __init__(self):
+        print(Player.CharacterSprites.keys())
+    

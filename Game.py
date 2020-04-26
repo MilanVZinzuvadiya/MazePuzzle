5ddba9ab8b3bcc4c78a099e68b69424f2e5462dd
Milan Zinzuvadiya <milanzinzuvadiya99@gmail.com>
2020-04-26 11:11:08 -0400

    Game Class Create & level files

---
+++ Game.py
@@ -0,0 +1,32 @@
+import pygame
+import Screen
+import sys
+
+class Game:
+
+    def __init__(self,width=800,height=600):
+        pygame.init()
+        self.WINWIDTH = width
+        self.WINHEIGHT = height
+        self.HALF_WINWIDTH = int(width/2)
+        self.HALF_WINHEIGHT = int(height/2)
+        
+        self.FPSLOCK = pygame.time.Clock()
+        self.DISPLAYSURF = pygame.display.set_mode((self.WINWIDTH,self.WINHEIGHT))
+        self.DISPLAYSURF.fill((205,255,255))
+        
+
+
+    def play(self):
+        pygame.init()
+        pygame.mixer.music.load('audio/ipsi.mp3')
+        pygame.mixer.music.play(-1)
+        scr=Screen.Screen(self.DISPLAYSURF,self.WINWIDTH,self.WINHEIGHT)
+        scr.starts()
+        scr.runLevels()
+
+    def GameExit():
+        pygame.quit()
+        sys.exit()
+    
+

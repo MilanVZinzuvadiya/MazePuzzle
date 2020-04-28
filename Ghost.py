9f9b93061861f4c0c0e7cdc0f64694a1ee0e868a
Milan Zinzuvadiya <milanzinzuvadiya99@gmail.com>
2020-04-27 23:35:23 -0400

    Ghost Class

---
+++ Ghost.py
@@ -0,0 +1,51 @@
+import pygame
+class Ghost:
+
+    def __init__(self,gh,speed,maxLimit):
+        self.type = gh['type']
+        self.pos = [gh['pos'][0],gh['pos'][1]]
+        self.speed = speed
+        self.interval = speed
+        self.limit = maxLimit -1
+        self.direction = 1
+
+    def getinfo(self):
+        print(self.type)
+        print(self.pos)
+        print(self.speed)
+        print(self.interval)
+        print(self.limit)
+
+    def Haunt(self,curTime):
+        if curTime > self.interval:
+            if self.type == 'V':
+                self.getNextPosV()
+            else:
+                self.getNextPosH()
+            
+            self.interval += self.speed
+
+            return True,self.pos
+        return False,self.pos
+
+    def getNextPosH(self):
+        print(self.pos[1],self.limit,self.pos[1])
+        if self.pos[1] < self.limit and self.pos[1] > 0:
+            self.pos[1] += self.direction
+        else:
+            self.pos[1] -= self.direction
+            self.direction = 0-(self.direction)
+
+    def getNextPosV(self):
+        print(self.pos[0],self.limit,self.pos[0])
+        if self.pos[0] < self.limit and self.pos[0] > 0:
+            self.pos[0] += self.direction
+        else:
+            self.pos[0] -= self.direction
+            self.direction = 0-self.direction
+    
+    def getSprite(self):
+        if self.type == 'V':
+            return pygame.image.load('Images/characters/catgirl.png')
+        else:
+            return pygame.image.load('Images/characters/horngirl.png')

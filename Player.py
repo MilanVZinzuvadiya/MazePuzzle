c295c0890b46219509e3b32093869d21d153172c
Milan Zinzuvadiya <milanzinzuvadiya99@gmail.com>
2020-04-28 02:09:31 -0400

    Player Class Update

--- Player.py
+++ Player.py
@@ -3,8 +3,6 @@ class Player:
     CharacterSprites = {
             'princess': pygame.image.load('Images/characters/princess.png'),
             'boy': pygame.image.load('Images/characters/boy.png'),
-            'catgirl': pygame.image.load('Images/characters/catgirl.png'),
-            'horngirl': pygame.image.load('Images/characters/horngirl.png'),
             'pinkgirl': pygame.image.load('Images/characters/pinkgirl.png')
         }
 
